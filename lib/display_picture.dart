4caf984ac9ea24366e0af833452fab51a91148de
sergey <seregakdg1@gmail.com>
2020-03-03 23:20:04 +0200

    add TextRecognizer - not work

--- lib/display_picture.dart
+++ lib/display_picture.dart
@@ -4,13 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 class DisplayPicture extends StatelessWidget{
+//  File imageFile = null;
+
   @override
   Widget build(BuildContext context) {
     final String picturePath = ModalRoute.of(context).settings.arguments;
 
     if (picturePath != null) {
+ //     imageFile = File(picturePath);
       return Scaffold(
-          appBar: AppBar(title: _buildTitle()),
+          appBar: AppBar(title: _buildTitle(picturePath)),
           body: Center(
             child: Image.file(File(picturePath)),
           ),
@@ -43,13 +46,32 @@ class DisplayPicture extends StatelessWidget{
   }
 }
 
-void _recognizeText(bool value) {
+void _recognizeText(String picturePath) {
+  final File imageFile = File(picturePath);
+  final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(imageFile);
+  final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
+/*
+  String text = visionText.text;
+  for (TextBlock block in visionText.blocks) {
+    final Rect boundingBox = block.boundingBox;
+    final List<Offset> cornerPoints = block.cornerPoints;
+    final String text = block.text;
+    final List<RecognizedLanguage> languages = block.recognizedLanguages;
 
+    for (TextLine line in block.lines) {
+      // Same getters as TextBlock
+      for (TextElement element in line.elements) {
+        // Same getters as TextBlock
+      }
+    }
+  }
+*/
+  textRecognizer.close();
 }
 
-  Widget _buildTitle() {
+  Widget _buildTitle([String picturePath]) {
     return RaisedButton(
-      onPressed: () => _recognizeText,
+      onPressed: () => _recognizeText(picturePath),
       child: Text('Recognize text'),
     );
   }
