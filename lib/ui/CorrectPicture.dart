13521216efd276538abc14613781ac4d7bc1d17c
sergey <seregakdg1@gmail.com>
2020-03-21 18:29:57 +0200

    work base version

--- lib/ui/CorrectPicture.dart
+++ lib/ui/CorrectPicture.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CorrectPicture extends StatefulWidget {
   final String routDisplayPicture;
@@ -11,22 +13,119 @@ class CorrectPicture extends StatefulWidget {
   CorrectPictureState createState() => CorrectPictureState();
 }
 
+enum AppState {
+  free,
+  picked,
+  cropped,
+}
+
 class CorrectPictureState extends State<CorrectPicture> with TickerProviderStateMixin{
+  AppState state;
+  File imageFile;
 
   @override
   void initState() {
     super.initState();
+    state = AppState.free;
   }
 
   @override
   Widget build(BuildContext context) {
     final String picturePath = ModalRoute.of(context).settings.arguments;
+//    imageFile = File(ModalRoute.of(context).settings.arguments);
 
     return Scaffold(
       appBar: AppBar(title: Text("Correct picture")),
       body: Center(
-        child: Image.file(File(picturePath)),
+        child: imageFile != null ? Image.file(imageFile) : Container(),
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrange,
+        onPressed: () {
+          if (state == AppState.free)
+            _pickImage();
+          else if (state == AppState.picked)
+            _cropImage();
+          else if (state == AppState.cropped)
+            _clearImage();
+        },
+        child: _buildButtonIcon(),
+      ),
+    );
+  }
+
+  Widget _buildButtonIcon() {
+    if (state == AppState.free)
+      return Icon(Icons.add);
+    else if (state == AppState.picked)
+      return Icon(Icons.crop);
+    else if (state == AppState.cropped)
+      return Icon(Icons.clear);
+    else
+      return Container();
+  }
+
+  Future<Null> _pickImage() async {
+    imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (imageFile != null) {
+      setState(() {
+        state = AppState.picked;
+      });
+    }
+  }
+
+  Future<Null> _cropImage() async {
+    File croppedFile = await ImageCropper.cropImage(
+        sourcePath: imageFile.path,
+        aspectRatioPresets: Platform.isAndroid
+            ? [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9
+        ]
+            : [
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio5x3,
+          CropAspectRatioPreset.ratio5x4,
+          CropAspectRatioPreset.ratio7x5,
+          CropAspectRatioPreset.ratio16x9
+        ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          title: 'Cropper',
+        )
+    );
+    if (croppedFile != null) {
+      imageFile = croppedFile;
+      setState(() {
+        state = AppState.cropped;
+      });
+    }
+  }
+
+  void _clearImage() {
+    imageFile = null;
+    setState(() {
+      state = AppState.free;
+    });
+  }
+
+}
+/*
+body: Center(
+child: Image.file(File(picturePath)),
+),
+
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.save),
         onPressed: () => Navigator.pushNamedAndRemoveUntil(
@@ -35,9 +134,4 @@ class CorrectPictureState extends State<CorrectPicture> with TickerProviderState
           (Route<dynamic> route) => false,
           arguments: picturePath,
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
-  }
-
-}
+ */
